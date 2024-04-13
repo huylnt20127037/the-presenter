cf4c157e1470e819ce47a3cbdacc92e72cda72c3
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-04-13 17:01:44 +0700

    Added sample sprite to pixi canvas

--- src/pixi.js
+++ src/pixi.js
@@ -1,6 +1,12 @@
 import * as PIXI from 'pixi.js'
+import SampleSprite from './assets/sample-sprite.png'
 
 const pixiApp = new PIXI.Application();
 await pixiApp.init({ width: 1080, height: 720 });
+
+// load the PNG asynchronously
+await PIXI.Assets.load(SampleSprite);
+let sprite = PIXI.Sprite.from(SampleSprite);
+pixiApp.stage.addChild(sprite);
 
 export default pixiApp
