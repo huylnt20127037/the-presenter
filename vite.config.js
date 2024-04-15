d65f215fd07fa479c2b1d12974c8b62fd19f312b
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-04-15 20:55:49 +0700

    Configured build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/the-presenter",
   plugins: [react(), svgr({
     exportAsDefault: true
   })],
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
 })
