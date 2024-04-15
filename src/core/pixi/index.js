ba1504b0e6ae0e67d8e1874bf20a22959a2e3508
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-04-15 21:10:35 +0700

    Changed favicon

--- src/core/pixi/index.js
+++ src/core/pixi/index.js
@@ -1,10 +1,7 @@
 import { Application, Color } from 'pixi.js';
-import createCharacter from './character';
 import { AppColor } from '../../theme';
 
 const pixiApp = new Application();
 await pixiApp.init({ width: 540, height: 600, backgroundColor: new Color(AppColor.secondary).toArray(), });
-
-// pixiApp.stage.addChild(await createCharacter(pixiApp));
 
 export default pixiApp
