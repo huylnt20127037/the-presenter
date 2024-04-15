8c6d47d318828d4d3c2535ab81eddc6854f78fa2
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-04-15 21:11:01 +0700

    Updates

--- assets/batchSamplersUniformGroup-BUm7Jf4X.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./index-BoTIEJDb.js";import{U as o}from"./colorToUniform-DSC6Zmo4.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
